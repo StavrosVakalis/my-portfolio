ab952a35155462af9bc5ae459caf34b6316b7f4f
Alexandros Tsiouris <alet@bunker-holding.com>
2022-10-26 11:17:55 +0300

    Update edit.js

--- pages/edit.js
+++ pages/edit.js
@@ -3,7 +3,7 @@ import Button from "../components/Button";
 import Header from "../components/Header";
 import { v4 as uuidv4 } from "uuid";
 import { useTheme } from "next-themes";
-import { useSession } from "next-auth";
+import { useSession } from "next-auth/react";
 
 // Data
 import yourData from "../data/portfolio.json";
